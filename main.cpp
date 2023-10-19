613b3ad6c1f2fead2daf9a3181c5cd6cba522a74
Jason Harris <lord.jtharris@gmail.com>
2023-10-19 16:10:23 -0500

    Fixing the errors for sake of having it be nice

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main()
     std::cout << "Enter your favorite number between 1 and 100: ";
     std::cin >> favorite_number;
     
-    std::cout << "That's my favorite number, too! << std::endl;
+    std::cout << "That's my favorite number, too!" << std::endl;
     
     return 0;
 }
