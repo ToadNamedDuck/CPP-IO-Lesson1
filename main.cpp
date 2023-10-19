8279ce1ff9d5e91af31677298ab4f8503520aa6b
Jason Harris <lord.jtharris@gmail.com>
2023-10-19 15:45:37 -0500

    Using cin, user types a number, program responds.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@ int main()
 {
     int favorite_number;
     std::cout << "Enter your favorite number between 1 and 100: ";
-
+    std::cin >> favorite_number;
+    
+    std::cout << "That's my favorite number, too!" << std::endl;
     
     return 0;
 }
